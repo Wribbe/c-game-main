c17dbd41c911d3b023cb46f3c87fae4206b2a553
wribbe <eng_steff@hotmail.com>
2016-11-21 18:49:27 +0100

    Closable window with static background working.

--- src/redone_boing.c
+++ src/redone_boing.c
@@ -7,6 +7,45 @@
 
 #include "linmath.h"
 
+void init(void) {
+    // Set background color.
+    glClearColor( 0.55f, 0.55f, 0.55f, 0.0f);
+}
+
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 int main(void) {
+
+    GLFWwindow * window;
+
+    // Init GLFW.
+    if (!glfwInit()) {
+        return EXIT_FAILURE;
+    }
+
+    window = glfwCreateWindow(400, 400, "Recreated Boing", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "[!] Could not create window, aborting.\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwSetWindowAspectRatio(window, 1, 1);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+    // Initialize values.
+    init();
+
+    while(!glfwWindowShouldClose(window)) {
+
+        display();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
     return EXIT_SUCCESS;
 }

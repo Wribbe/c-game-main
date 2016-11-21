5ffe26b5d3313e8cc07ba483fd990e6066e2bec4
wribbe <eng_steff@hotmail.com>
2016-11-21 19:25:10 +0100

    Print hello message from thread.

--- src/redone_boing.c
+++ src/redone_boing.c
@@ -1,23 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <pthread.h>
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 #include "linmath.h"
 
-void init(void) {
+void init(void)
+{
     // Set background color.
     glClearColor( 0.55f, 0.55f, 0.55f, 0.0f);
 }
 
-void display(void) {
+void display(void)
+{
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-int main(void) {
+typedef struct {
+    GLFWwindow * window;
+    void (* GLFWkeyfun)(GLFWwindow *, int, int, int, int);
+    int * keymap;
+} key_callback_data;
 
+void callback_key(
+                  GLFWwindow * window,
+                  int key,
+                  int scancode,
+                  int action,
+                  int mods
+                 )
+{
+    printf("Key: %d\n", key);
+}
+
+void * setup_callback(void * data) {
+    /* Worker function for thread for setting up key callback. */
+    printf("Hello from thread.\n");
+    return NULL;
+}
+
+
+int main(void)
+{
     GLFWwindow * window;
 
     // Init GLFW.
@@ -35,6 +62,20 @@ int main(void) {
 
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+    int key_map[512];
+    key_callback_data callback_data = {
+        window,
+        callback_key,
+        key_map,
+    };
+
+    pthread_t key_callback_thread;
+
+    pthread_create(&key_callback_thread, NULL, setup_callback, &callback_data);
+
+    // Set key callback function for window.
+    glfwSetKeyCallback(window, callback_key);
 
     // Initialize values.
     init();

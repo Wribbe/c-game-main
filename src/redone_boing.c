3498939506bc3808dc9375d88241fcdc074c470b
wribbe <eng_steff@hotmail.com>
2016-11-21 18:29:50 +0100

    Add src and redone boing stub.

---
+++ src/redone_boing.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "linmath.h"
+
+int main(void) {
+    return EXIT_SUCCESS;
+}

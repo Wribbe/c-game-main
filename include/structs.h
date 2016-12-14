a7fa5458a585f578727a67899f10ce889e1c85b8
wribbe <eng_steff@hotmail.com>
2016-12-14 13:57:54 +0100

    Sort out struct ordering.

--- include/structs.h
+++ include/structs.h
@@ -6,10 +6,24 @@
 #include "maths/math_utils.h"
 #include "globals/globals.h"
 
-typedef enum event_action_type {
+// Forward declaration of submit_type for wrapper function.
+union submit_type;
+struct Command_Packet;
+struct component;
+struct s_flag;
+struct s_float;
+
+enum event_action_type {
     PASSTHROUGH,
     BLOCKING,
 } event_action_type;
+
+enum uniform_type {
+    /* Strip the gl part of the glUniform* functions used. */
+    UniformMatrix4fv,
+    Uniform1f,
+    NUM_TYPES,
+};
 
 typedef enum comparison_type {
     GT,
@@ -18,13 +32,6 @@ typedef enum comparison_type {
     LTEQ,
     EQ,
 } comparison_type;
-
-enum uniform_type {
-    /* Strip the gl part of the glUniform* functions used. */
-    UniformMatrix4fv,
-    Uniform1f,
-    NUM_TYPES,
-};
 
 typedef struct Point_Data {
     size_t rows;
@@ -37,6 +44,24 @@ typedef struct Attrib_Pointer_Info {
     GLint size;
     float * offset;
 } Attrib_Pointer_Info;
+
+typedef float (*mod_function)(float current, float input, void * data);
+
+typedef bool (*logic_function)(float left_side,
+                               comparison_type comp,
+                               float right_side);
+
+typedef struct Action_Logic_Data{
+    // Logic comparison data.
+    logic_function logic_function;
+    float comparison_value;
+    comparison_type comparison_type;
+    // Regular function to use if logic does not trigger.
+    mod_function regular_function;
+    // Alternative function that is run if the logic triggers.
+    mod_function replacement_function;
+    float replacement_value;
+} Action_Logic_Data;
 
 typedef struct VBO {
     GLuint vbo;
@@ -55,52 +80,13 @@ typedef struct VAO {
     Attrib_Pointer_Info * attrib_list;
 } VAO;
 
-typedef float (*mod_function)(float current, float input, void * data);
-
-typedef struct Command_Packet {
-    float * variable;
-    float value;
-    int lifetime;
-    event_action_type type;
-    mod_function mod_function;
-    void * data;
-    struct Command_Packet * next;
-    struct Command_Packet * sub_commands;
-    struct Command_Packet * last_sub;
-} Command_Packet;
-
-typedef struct Command_Input {
-    float * variable;
-    float value;
-    mod_function mod_function;
-    void * data;
-    int lifetime;
-    event_action_type type;
-} Command_Input;
-
-typedef bool (*logic_function)(float left_side,
-                               comparison_type comp,
-                               float right_side);
-
-typedef struct Action_Logic_Data{
-    // Logic comparison data.
-    logic_function logic_function;
-    float comparison_value;
-    comparison_type comparison_type;
-    // Regular function to use if logic does not trigger.
-    mod_function regular_function;
-    // Alternative function that is run if the logic triggers.
-    mod_function replacement_function;
-    float replacement_value;
-} Action_Logic_Data;
-
 struct component {
     char * id;
     m4 transformation;
     VAO * vao;
     float modifiers[3];
-    Command_Packet * command_list;
-    Command_Packet * last_command;
+    struct Command_Packet * command_list;
+    struct Command_Packet * last_command;
     struct component * next;
     uint64_t flags;
 };
@@ -118,5 +104,39 @@ struct collision_bound_data {
     void (*flag_operation)(struct component * component, enum flag_type flag);
     enum flag_type flag;
 };
+
+struct s_flag {
+    void (*function)(struct component * component, enum flag_type flag);
+    enum flag_type flag;
+    struct component * component;
+    int lifetime;
+    enum event_action_type action_type;
+    void (*wrapper_function)(union submit_type * type);
+    void (*remove)(union submit_type * type);
+};
+
+struct s_float {
+    float (*function)(float input, float value, void * data);
+    float value;
+    float * variable;
+    int lifetime;
+    void * data;
+    enum event_action_type action_type;
+    float (*wrapper_function)(union submit_type * type);
+    void (*remove)(union submit_type * type);
+};
+
+union submit_type {
+    struct s_flag s_flag;
+    struct s_float s_float;
+};
+
+struct Command_Packet {
+    union submit_type type;
+    struct Command_Packet * next;
+    struct Command_Packet * sub_command;
+    struct Command_Packet * last_sub;
+};
+
 
 #endif

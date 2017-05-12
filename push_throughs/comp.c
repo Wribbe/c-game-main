d7fb7157c8bbb04270e4d3bf2272b637459c275b
wribbe <eng_steff@hotmail.com>
2017-05-12 18:38:31 +0200

    Small build-program done.

---
+++ push_throughs/comp.c
@@ -0,0 +1,77 @@
+#include <dirent.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define SIZE(x) sizeof x / sizeof x[0]
+
+const char * SOURCE_EXTENSION = ".c";
+const char * PATH_SEP = "/";
+
+enum dirpos {
+    BIN,
+    TOTAL_DIRS,
+};
+const char * dirs[TOTAL_DIRS];
+
+int endswith(const char * string, const char * ending)
+{
+    char * rightmost_dot = strrchr(string, '.');
+    if(rightmost_dot && strcmp(rightmost_dot, ending) == 0) {
+        return(rightmost_dot - string);
+    }
+    return 0;
+}
+
+void setup_folders(void)
+{
+    dirs[BIN] = "bin";
+    int mode = 0700;
+
+    struct stat st = {0};
+    for (size_t i = 0; i<TOTAL_DIRS; i++) {
+        if(stat(dirs[i], &st) == -1) {
+            mkdir(dirs[i], mode);
+        }
+    }
+}
+
+void run(const char * format, ...)
+{
+    size_t command_size = 512;
+    char buffer[command_size];
+
+    va_list args;
+
+    va_start(args, format);
+    int chars_written = vsnprintf(buffer, command_size-1, format, args);
+    va_end(args);
+    buffer[chars_written] = '\0';
+    printf("command == %s\n", buffer);
+
+    system(buffer);
+}
+
+
+int main(void)
+{
+    setup_folders();
+    struct dirent * dir = NULL;
+    DIR * d = opendir(".");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            const char * dir_name = dir->d_name;
+            int dot_pos = endswith(dir_name, SOURCE_EXTENSION);
+            if (dot_pos) {
+                run("gcc %s -o %s%s%.*s", dir_name, dirs[BIN], PATH_SEP, dot_pos, dir_name);
+            }
+        }
+        closedir(d);
+    }
+    return(EXIT_SUCCESS);
+}
---
+++ push_throughs/gimmie_a_window.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "glfw3.h"
+
+int main(void)
+{
+    printf("Hello World.\n");
+}

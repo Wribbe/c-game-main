3940a49ea7d6a0bd518d64976190f3bc69d3b01c
wribbe <eng_steff@hotmail.com>
2017-08-29 21:21:40 +0200

    Add gl3w for GL-wrangling, instead of GLEW.

--- active/main.c
+++ active/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "gl3w.h"
 #include <GLFW/glfw3.h>
 
 int
@@ -9,17 +10,24 @@ main(void)
     GLFWwindow * window;
 
     if (!glfwInit()) {
+        fprintf(stderr, "Could not initialize glfw, aborting.\n");
         return EXIT_FAILURE;
     }
 
     window = glfwCreateWindow(640, 480, "HELLO WORLD", NULL, NULL);
     if (!window) {
+        fprintf(stderr, "Could not create window, aborting.\n");
         glfwTerminate();
         return EXIT_FAILURE;
     }
 
     /* Make window context current one. */
     glfwMakeContextCurrent(window);
+
+    if (gl3wInit()) {
+        fprintf(stderr, "Could not initialize gl3w, aborting.\n");
+        return EXIT_FAILURE;
+    }
 
     /* Loop until the user closes window. */
     while (!glfwWindowShouldClose(window)) {
@@ -33,4 +41,6 @@ main(void)
         /* Poll events. */
         glfwPollEvents();
     }
+
+    return EXIT_SUCCESS;
 }

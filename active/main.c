f86d5d4f33b2f0bd1765d90f77a4544ac47f4df7
wribbe <eng_steff@hotmail.com>
2017-08-30 08:42:03 +0200

    Add file-reading functionality.

--- active/main.c
+++ active/main.c
@@ -4,6 +4,32 @@
 #include "gl3w.h"
 #include <GLFW/glfw3.h>
 
+char *
+read_file(const char * filepath)
+{
+    FILE * fpointer =  fopen(filepath, "r");
+    if (fpointer == NULL) {
+        fprintf(stderr, "Could not find file %s, aborting.\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(fpointer, 0, SEEK_END);
+    size_t size = ftell(fpointer)+1;
+    rewind(fpointer);
+
+    char * data = malloc(size);
+    if (data == NULL) {
+        fprintf(stderr, "Could not allocate memory for reading file,"
+                " aborting.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fread(data, size, 1, fpointer);
+    data[size-1] = '\0';
+
+    return data;
+}
+
 int
 main(void)
 {
@@ -41,6 +67,8 @@ main(void)
     printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION),
             glGetString(GL_SHADING_LANGUAGE_VERSION));
 
+    char * test = read_file("vert_simple.glsl");
+    printf("test: %s\n", test);
 
     /* Loop until the user closes window. */
     while (!glfwWindowShouldClose(window)) {
@@ -54,6 +82,9 @@ main(void)
         /* Poll events. */
         glfwPollEvents();
     }
+
+    glfwTerminate();
+    free(test);
 
     return EXIT_SUCCESS;
 }

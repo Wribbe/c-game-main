b7f3c1293919d922242e05721120205ca2c36045
wribbe <eng_steff@hotmail.com>
2017-11-14 12:59:39 +0100

    Setup shader in collisions.

--- active/collisions.c
+++ active/collisions.c
@@ -55,10 +55,94 @@ setup_glfw(void)
     return window;
 }
 
+const GLchar * source_shader_vertex =
+"#version 330 core\n"
+"\n"
+"layout (location=0) in vec3 vertex_data;\n"
+"\n"
+"void main() {\n"
+"  gl_Position = vec4(vertex_data, 1.0f);\n"
+"}\n";
+
+const GLchar * source_shader_fragment =
+"#version 330 core\n"
+"\n"
+"void main() {\n"
+"  gl_FragColor = vec4(1.0f);\n"
+"}\n";
+
+GLuint
+setup_shaders()
+{
+    /* Create vertex and fragment shaders. */
+    GLuint shader_vertex = glCreateShader(GL_VERTEX_SHADER);
+    GLuint shader_fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+    /* Load correct source into each shader. */
+    glShaderSource(shader_vertex, 1, &source_shader_vertex, NULL);
+    glShaderSource(shader_fragment, 1, &source_shader_fragment, NULL);
+
+    /* Compile shaders. */
+    glCompileShader(shader_vertex);
+    glCompileShader(shader_fragment);
+
+    /* Prepare error data structures. */
+    GLint operation_successful = 0;
+    GLsizei size_error_buffer = 1024;
+    GLchar buffer_error_message[size_error_buffer];
+
+    /* Check compilation status of vertex shader. */
+    glGetShaderiv(shader_vertex, GL_COMPILE_STATUS, &operation_successful);
+    if (!operation_successful) {
+        glGetShaderInfoLog(shader_vertex, size_error_buffer, NULL,
+                buffer_error_message);
+        fprintf(stderr, "Error on compiling vertex shader: %s\n\n%s\n",
+                buffer_error_message, source_shader_vertex);
+    }
+
+    /* Check compilation status of fragment shader. */
+    glGetShaderiv(shader_fragment, GL_COMPILE_STATUS, &operation_successful);
+    if (!operation_successful) {
+        glGetShaderInfoLog(shader_fragment, size_error_buffer, NULL,
+                buffer_error_message);
+        fprintf(stderr, "Error on compiling fragment shader: %s\n\n%s\n",
+                buffer_error_message, source_shader_fragment);
+    }
+
+    /* Create shader program. */
+    GLuint program_shader = glCreateProgram();
+
+    /* Attach compiled shaders. */
+    glAttachShader(program_shader, shader_vertex);
+    glAttachShader(program_shader, shader_fragment);
+
+    /* Link the shader program. */
+    glLinkProgram(program_shader);
+
+    /* Check link status. */
+    glGetProgramiv(program_shader, GL_LINK_STATUS, &operation_successful);
+    if (!operation_successful) {
+        glGetProgramInfoLog(program_shader, size_error_buffer, NULL,
+                buffer_error_message);
+        fprintf(stderr, "Error on linking the shader program: %s\n",
+                buffer_error_message);
+    }
+
+    /* Delete compiled shaders. */
+    glDeleteShader(shader_vertex);
+    glDeleteShader(shader_fragment);
+
+    return program_shader;
+}
+
+
 int
 main(void)
 {
     GLFWwindow * window = setup_glfw();
+
+    GLuint program_shader = setup_shaders();
+    glUseProgram(program_shader);
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -67,6 +151,8 @@ main(void)
 
         /* Poll events. */
         glfwPollEvents();
+
+        /* Draw. */
 
         /* Swap. */
         glfwSwapBuffers(window);
